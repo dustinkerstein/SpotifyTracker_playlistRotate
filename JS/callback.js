504d382e765b561a5531e3d28fcb207d97560885
Stan van Baarsen <stanvanbaarsen@hotmail.com>
2022-05-25 12:21:00 +0200

    restructure files

---
+++ JS/callback.js
@@ -0,0 +1,20 @@
+	onload = function(){
+		url = location.href;
+		at = (url.split("access_token=")[1]||"").split("&")[0];
+		ei = (url.split("expires_in=")[1]||"").split("&")[0];
+		if(at && ei){
+			if('localStorage' in window){
+				localStorage.setItem("token_expiration", "" + new Date(+new Date() + (+ei)*1000));
+				localStorage.setItem("access_token", at);
+				location = location.href.split("/SpotifyTracker").length > 1 ? "../SpotifyTracker/" : "../"
+			}else{
+				erreur();
+			}
+		}else{
+			erreur();
+		}
+	}
+
+	function erreur(){
+		document.write("Er is iets mis gegaan. <a href='../SpotifyTracker/'>Terug naar home</a>");
+	}
